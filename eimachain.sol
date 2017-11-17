18ae7e1e3cf72e794f8d3ce53d4b5a97d22cecf0
unknown <epo@klintra.fo>
2017-11-17 11:20:22 +0100

    Combined Array and Main branch solution

--- eimachain.sol
+++ eimachain.sol
@@ -4,30 +4,35 @@ import "./certificate.sol";
 
 contract EiMaChain {
 
-    mapping(bytes32 => Certificate) certificates;
+    mapping(bytes32 => uint256) pointers;
+    Certificate[] certificates;
+
+    function EiMaChain() public {
+        certificates.push(Certificate(0x0));
+    }
 
     /// Add a new certificate to chain.
     function addCertificate(string url) public returns (bool _allowed) {
         bytes32 hashedInput = keccak256(url);
-        Certificate oldCert = certificates[hashedInput];
-        Certificate newCert = new Certificate(url, msg.sender, oldCert);
-        if (address(oldCert) == 0x0) {
-            certificates[hashedInput] = newCert;
+        Certificate oldCert = certificates[pointers[hashedInput]];
+
+        if (pointers[hashedInput] == 0) {
+            pointers[hashedInput] = certificates.push(new Certificate(url, msg.sender, Certificate(0x0)));
             return true;
         } else {
             if (oldCert.getOwner() == msg.sender) {
-                certificates[hashedInput] = newCert;
+                certificates[pointers[hashedInput]] = new Certificate(url, msg.sender, oldCert);
                 return true;
             }
-            return false;
         }
+        return false;
     }
     
     function getCertificate(string url) public view returns (Certificate) {
         bytes32 hashedInput = keccak256(url);
-        return certificates[hashedInput];
+        return certificates[pointers[hashedInput]];
     }
-    
+
     function transferOwnership(Certificate cert, address newOwner) public returns (bool _allowed) {
         if (msg.sender != cert.getOwner()) 
             return false;
